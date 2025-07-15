26839cb0100991493014aae194feaefe583dd3a7
uc-cjkhoh <cj_khoh@unifiedcomms.com>
2025-07-15 14:11:13 +0800

    Update show_prefix_translation_failures_rate.sql

--- RVS HSC and ICC/show_prefix_translation_failures_rate.sql
+++ RVS HSC and ICC/show_prefix_translation_failures_rate.sql
@@ -1,18 +1,20 @@
 -- Objection: For each country's failed/rejected call, find out the msisdn's prefix distribution in percentage or count 
 
 WITH
-    -- Count inbound ICC traffic, then select the top 10 countries with highest traffic
+    -- Count inbound ICC traffic, then select the top N countries with highest traffic
     target_countries AS (
         SELECT 
+            clg_mcc_ref,
             cld_mcc_ref,
             COUNT(1) as traffic
         FROM
             roam352_report.data_cdr
         WHERE
             service_type = 301              -- ICC service
-            -- AND status = '10001f'        -- rejected tx (ACM ?) 
-            AND cld_mcc_ref != clg_mcc_ref  -- caller and callee not in same nation    
+            AND status = '10001f'           -- rejected tx (ACM ?) 
+            AND par_bound_type = 1          -- inbound 
         GROUP BY
+            clg_mcc_ref,
             cld_mcc_ref
         ORDER BY 
             traffic
@@ -22,6 +24,7 @@ WITH
     -- Then, count the number of failed colling having the same 1st digit, 1st and 2nd digitss and so on
     base_tbl AS (
         SELECT 
+            clg_mcc_ref,
             cld_mcc_ref, 
             LEFT(cld_num, 1) as p1,
             LEFT(cld_num, 2) as p2,
@@ -31,10 +34,11 @@ WITH
             roam352_report.data_cdr 
         WHERE
             service_type = 301                                              -- ICC service
-            AND status = '10001f'                                           -- rejected tx (ACM ?) 
-            AND cld_mcc_ref != clg_mcc_ref                                  -- caller and callee not in same nation    
-            AND cld_mcc_ref in (SELECT cld_mcc_ref FROM target_countries)   -- target countries 
+            AND status = '10001f'                                           -- rejected tx (ACM ?)
+            AND par_bound_type = 1                                          -- inbound    
+            AND cld_mcc_ref in (460)                                        -- target countries 
         GROUP BY 
+            clg_mcc_ref,
             cld_mcc_ref,
             p1,
             p2,
@@ -43,14 +47,16 @@ WITH
     ) 
 
 
+
+
 SELECT * FROM (
-    SELECT cld_mcc_ref, p1 as prefix, COUNT(*) as total_count FROM base_tbl GROUP BY cld_mcc_ref, p1
+    SELECT clg_mcc_ref, cld_mcc_ref, p1 as prefix, COUNT(*) as total_count FROM base_tbl GROUP BY clg_mcc_ref, cld_mcc_ref, p1
     UNION ALL
-    SELECT cld_mcc_ref, p2 as prefix, COUNT(*) as total_count FROM base_tbl GROUP BY cld_mcc_ref, p2
+    SELECT clg_mcc_ref, cld_mcc_ref, p2 as prefix, COUNT(*) as total_count FROM base_tbl GROUP BY clg_mcc_ref, cld_mcc_ref, p2
     UNION ALL
-    SELECT cld_mcc_ref, p3 as prefix, COUNT(*) as total_count FROM base_tbl GROUP BY cld_mcc_ref, p3
+    SELECT clg_mcc_ref, cld_mcc_ref, p3 as prefix, COUNT(*) as total_count FROM base_tbl GROUP BY clg_mcc_ref, cld_mcc_ref, p3
     UNION ALL
-    SELECT cld_mcc_ref, p4 as prefix, COUNT(*) as total_count FROM base_tbl GROUP BY cld_mcc_ref, p4
+    SELECT clg_mcc_ref, cld_mcc_ref, p4 as prefix, COUNT(*) as total_count FROM base_tbl GROUP BY clg_mcc_ref, cld_mcc_ref, p4
 ) tbl
     WHERE total_count > 1
-    ORDER BY cld_mcc_ref, total_count DESC
+    ORDER BY clg_mcc_ref, cld_mcc_ref, total_count DESC
